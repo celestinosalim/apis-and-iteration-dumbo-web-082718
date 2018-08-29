8c6e88c34bb4c1993f367e5c94cbf8b3d616d099
celestinosalim <celestino.salim@gmail.com>
2018-08-29 11:04:51 -0400

    modifified

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,10 +6,26 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
+  movies = response_hash["results"]
+
+  found_character = movies.find do |character_hash|
+    character == character_hash["name"].downcase
+  end
+  if found_character
+    return found_character['films']
+  else
+    puts "Not Found"
+    return nil
+end
+end
+
+
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
+  # This is done for educational purposes. This is not typically done in code.
+# binding.pry
+# 0
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -19,18 +35,47 @@ def get_character_movies_from_api(character)
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
+
   #  of movies by title. play around with puts out other info about a given film.
+  # response_hash["results"][0]["films"]
+# binding.pry
+
+
+def names
+  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_hash = JSON.parse(response_string)
+
+  people = response_hash["results"]
+  people.collect do |person|
+  person["name"]
+  end
 end
+
 
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  # binding.pry
+  films_hash.collect do |url|
+    response_string = RestClient.get(url)
+    response_hash = JSON.parse(response_string)
+    # puts response_hash["title"]
+# binding.pry
+  end
+
 end
+
 
 def show_character_movies(character)
-  films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+    films_array = get_character_movies_from_api(character)
+    # binding.pry
+  if films_array
+    print_movies(films_array)
+end
 end
 
+
+
+
+# puts show_character_movies("R2-D2")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,17 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome User!"
 end
 
 def get_character_from_user
+
   puts "please enter a character"
+
+name = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+
+# def get_more_info
+#   puts "enter movie"
+#
+# movie = gets.chomp.downcase
